d5ac20041ae8f9aabe0853a90e5e2d95798f7ea7
Andrew Brodko <work.andrewbrodko@gmail.com>
2020-02-25 17:21:35 +0300

    React root component rebase

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import 'normalize.css';
 import './index.css';
-import App from './App';
+
+import rootReducer from './reducers';
+import VTaskList from './containers/VTaskList';
+import VDescription from './containers/VDescription';
+import VFooter from './containers/VFooter'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <VTaskList />
+        <VDescription />
+        <VFooter />
+    </Provider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
