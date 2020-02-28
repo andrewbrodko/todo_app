122240c6f9347b6a392f8cb47e8eda9ea7ebccef
Andrew Brodko <work.andrewbrodko@gmail.com>
2020-02-28 21:18:49 +0300

    Task description extended editing

--- src/components/Description.js
+++ src/components/Description.js
@@ -3,13 +3,25 @@ import PropTypes from 'prop-types'
 import './Description.css';
 
 function Description({ task, submitRef, submitTask }) {
+    const dateMax = '2030-01-01';
+    const dateMin = new Date().toISOString().split('T')[0];
+
     const ref = useRef();
     const refH = useRef();
     const refT = useRef();
+    const refDD = useRef();
+    const refHH = useRef();
     useEffect(() => {
-        submitRef(ref);
+        var date = new Date(task.date.getTime() - task.date.getTimezoneOffset() * 60000);
+        if (task.id) {
+            submitRef(ref);
+        }
+        var time = date.toISOString().split('T')[1];
+
         refH.current.value = task.head;
         refT.current.value = task.text;
+        refDD.current.value = date.toISOString().split('T')[0];
+        refHH.current.value = [ time.split(':')[0], time.split(':')[1] ].join(':');
     });
 
     const submitHead = e => {
@@ -18,13 +30,38 @@ function Description({ task, submitRef, submitTask }) {
     const submitText = e => {
         submitTask({ id: task.id, text: e.target.value });
     };
+    const submitDate = e => {
+        if (refDD.current.valueAsDate !== null &&
+            refDD.current.valueAsDate.getTime() < new Date(dateMax).getTime() &&
+            refDD.current.valueAsDate.getTime() > new Date(dateMin).getTime()) {
+            var date = refDD.current.valueAsDate.getTime();
+            var time = refHH.current.valueAsDate.getTime() + refHH.current.valueAsDate.getTimezoneOffset() * 60000;
+            submitTask({ id: task.id, date: new Date(date + time) });
+        }
+    };
 
     return (
         <div className="description" ref={ref}>
-            <input type="text" ref={refH} onChange={submitHead} ></input>
-            <textarea ref={refT} onChange={submitText} ></textarea>
-            <div onClick={ () => window.scroll(0, 0) } hidden={true} >back</div>
-        </div>);
+            <input type="text" ref={refH} onChange={submitHead} disabled={task.id === undefined || task.completed}></input>
+            <textarea ref={refT} onChange={submitText} disabled={task.id === undefined || task.completed}></textarea>
+            <input
+                type="date"
+                ref={refDD}
+                onChange={submitDate}
+                required={true}
+                max={dateMax}
+                hidden={task.id === undefined}
+                disabled={task.id === undefined || task.completed}></input>
+            <input
+                type="time"
+                ref={refHH}
+                onChange={submitDate}
+                required={true}
+                hidden={task.id === undefined}
+                disabled={task.id === undefined || task.completed}></input>
+            <div onClick={ () => window.scroll(0, 0) } hidden={true}>back</div>
+        </div>
+    );
 }
 
 Description.propTypes = {
--- src/containers/VDescription.js
+++ src/containers/VDescription.js
@@ -3,9 +3,16 @@ import { submitRef, submitTask } from '../actions'
 import Description from '../components/Description';
 
 export default connect(
-    state => ({
-        task: state.repository.filter(t => t.id === state.description.id)[0]
-    }),
+    state => {
+        var task = state.repository.filter(t => t.id === state.description.id)[0];
+        if (task) {
+            return { task: task };
+        } else {
+            var date = new Date();
+            date.setDate(date.getDate() + 1);
+            return { task : { head: ' ', text: ' ', date: date } };
+        }
+    },
     dispatch => ({
         submitRef: ref => dispatch(submitRef(ref)),
         submitTask: task => dispatch(submitTask(task))
